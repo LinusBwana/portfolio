e9b7bacae4fe2d5ed6378fa4c81df0c811b2a6ee
Linus Bwana <linusbwana49@gmail.com>
2026-01-27 00:58:36 +0300

    update projects

--- my-portfolio/src/pages/Projects.jsx
+++ my-portfolio/src/pages/Projects.jsx
@@ -25,7 +25,7 @@ function Projects() {
       title: 'Fullstack Task Manager',
       description: 'A task manager built to track my todo list',
       technologies: ['Python', 'Django', 'DRF', 'React.js', 'PostgreSQL', 'Bootstrap'],
-      link: '#'
+      link: 'https://job-board-platform.up.railway.app/'
     }
   ];
 
